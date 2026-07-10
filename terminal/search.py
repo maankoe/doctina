e02a4ed854d04514c7b8d21be42eeeb9e8283f01
maankoe <maankoe@bernard>
2026-07-10 22:06:16 +0200

    working 'search'

--- terminal/search.py
+++ terminal/search.py
@@ -16,14 +16,19 @@ class Search:
         )
         self._fill_data_index = 0
         self._insert_pos = 0
+        if self._reversed:
+            self._next_index()
 
     def reverse(self):
-        pass
+        self._reversed = True
+        self._next_index()
+        self._next_index()
 
     def next(self):
         while True:
             x = self._fill_data[self._fill_data_index]
             found = f"{x[:self._insert_pos]}{self._s_chars}{x[self._insert_pos:]}"
+            print(self._insert_pos, self._fill_data_index)
             findex = decode_index(found)
             self._next_index()
             #if not _is_valid_uuid4(findex):
@@ -36,24 +41,26 @@ class Search:
 
 
     def _next_index(self):
+        #print(self._insert_pos, self._fill_data_index, self._fill_data.num_bits()/4, self._reversed)
         if self._reversed:
             if self._insert_pos == 0:
-                self._insert_pos = self._fill_data.num_bits()/4 
+                self._insert_pos = self._fill_data.num_bits()//4 
                 if self._fill_data_index == 0:
                     self._fill_data_index = self._fill_data.size() - 1 
                 else:
                     self._fill_data_index -= 1
             else:
                 self._insert_pos -= 1
-        #print(self._insert_pos, self._fill_data_index, self._fill_data.num_bits()/4)
-        if self._insert_pos >= self._fill_data.num_bits()/4:
-            self._insert_pos = 0
-            if self._fill_data_index >= self._fill_data.size():
-                self._fill_data_index = 0
-            else:
-                self._fill_data_index += 1
         else:
-            self._insert_pos += 1
+            if self._insert_pos >= self._fill_data.num_bits()/4:
+                self._insert_pos = 0
+                if self._fill_data_index >= self._fill_data.size():
+                    self._fill_data_index = 0
+                else:
+                    self._fill_data_index += 1
+            else:
+                self._insert_pos += 1
+        #print(self._insert_pos, self._fill_data_index, self._fill_data.num_bits()/4)
 
 
 if __name__ == "__main__":
@@ -61,6 +68,13 @@ if __name__ == "__main__":
     data = Dataset(num_bits=16, encode_index=create_index_encoder(16))
 
     search = Search(data, "aaa")
-    for i, x in enumerate(range(33)):
+    for i, x in enumerate(range(37)):
         fi = search.next()
         print(data[fi], i, fi, data[fi])
+    search.reverse()
+    print("------")
+    for i, x in enumerate(range(37)):
+        fi = search.next()
+        print(data[fi], i, fi, data[fi])
+
+
